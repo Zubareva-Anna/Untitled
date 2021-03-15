cd125eef5c22f530c511dd191307dc462e8864a4
Zubareva-Anna <a84.zubareva@gmail.com>
2021-03-15 19:03:55 +0300

    11

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // TODO: подставлять номер карты нужно сюда между двойными кавычками, без пробелов
-        String number = "5452913423099438";
+        String number = "30257351870902";
         System.out.println(String.format("Result is %s", isValidCardNumber(number) ? "OK" : "FAIL"));
     }
 
